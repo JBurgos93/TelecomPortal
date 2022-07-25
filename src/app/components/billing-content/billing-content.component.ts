3c9b6a4767856ec535901028c35480f0de465b59
JBurgos93 <jburgos@skillstorm.com>
2022-07-25 15:32:04 -0400

    Updated Billing to be a modal.

--- src/app/components/billing-content/billing-content.component.ts
+++ src/app/components/billing-content/billing-content.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-billing-content',
-  templateUrl: './billing-content.component.html',
-  styleUrls: ['./billing-content.component.css']
+    selector: 'app-billing-content',
+    templateUrl: './billing-content.component.html',
+    styleUrls: ['./billing-content.component.css']
 })
 export class BillingContentComponent implements OnInit {
 
-  constructor() { }
+    public displayStyle: String = "none";
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void { }
+
+    showBilling = () => this.displayStyle = "block";
+    hide = () => this.displayStyle = "none";
+    
 }
--- src/app/components/billing-page-content/billing-page-content.component.ts
+++ src/app/components/billing-page-content/billing-page-content.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PhonePlan } from 'src/app/models/phone-plan.model';
+import { UserPlansService } from 'src/app/services/user-plans.service';
 
 @Component({
-  selector: 'app-billing-page-content',
-  templateUrl: './billing-page-content.component.html',
-  styleUrls: ['./billing-page-content.component.css']
+    selector: 'app-billing-page-content',
+    templateUrl: './billing-page-content.component.html',
+    styleUrls: ['./billing-page-content.component.css']
 })
 export class BillingPageContentComponent implements OnInit {
 
-  constructor() { }
+    public phonePlans: PhonePlan[] = [];
+    public total: number = 0.0;
+    public rowNumber: number = 1;
 
-  ngOnInit(): void {
-  }
+    constructor(private userPlansService: UserPlansService) {
+        this.calculateTotalBill();
+    }
 
+    ngOnInit(): void {
+        this.phonePlans = this.userPlansService.getActivePlans();
+        this.calculateTotalBill();
+    }
+
+    calculateTotalBill(){
+        this.total = 0;
+        this.phonePlans.forEach(plan => {
+            //this.total += plan.cost;
+        });
+    }
 }
